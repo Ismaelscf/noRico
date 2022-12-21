3cf456837ddfd2cb93a2770df80777532eb1d27d
Leandro Edson Silva de Araujo <leandro.araujo@stc.ma.gov.br>
2022-12-21 00:52:11 -0300

    Criação de UserService e UserRepository

---
+++ app/Services/UserService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\UserRepository;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use InvalidArgumentException;
+
+class UserService
+{
+    protected $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+}
